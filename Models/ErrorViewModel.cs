e649caf61f25756f5ea9ff5c0420603c0b5b303b
Sevgi Bilgiç <sevgibilgic60@gmail.com>
2025-11-29 01:08:01 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuestionPlatform2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
